d97e92eb6811b12e2c371a12fadcd2275cd89182
harunozer <harunozer@hotmail.com.tr>
2017-09-26 23:12:57 +0300

    Xamarin Resource Tanımlama

---
+++ ResourceOrnekler/ResourceOrnekler/ResourceOrnekler/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ResourceOrnekler
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            // The root page of your application
+            var buttonStyle = new Style(typeof(Button))
+            {
+                Setters = {
+                    new Setter { Property = Button.TextColorProperty,   Value = Color.Blue }
+                    ,new Setter { Property = Button.BackgroundColorProperty,   Value = Color.Silver }
+                }
+            };
+            
+            App.Current.Resources.Add("GlobalButtonStyleCS", buttonStyle);
+            
+            MainPage = new StylePage();
+        }
+    }
+}
---
+++ ResourceOrnekler/ResourceOrnekler/ResourceOrnekler/StylePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ResourceOrnekler
+{
+    public partial class StylePage : ContentPage
+    {
+        public StylePage()
+        {
+            InitializeComponent();
+
+            var buttonStyle = new Style(typeof(Button))
+            {
+                Setters = {
+                    new Setter { Property = Button.TextColorProperty,   Value = Color.Red }
+                }
+            };
+
+            button1.Style = buttonStyle;
+        }
+
+        private void ThemeDegistir(object sender, EventArgs e)
+        {
+            Application.Current.Resources["Theme1Title"] = Application.Current.Resources["Theme2Title"];
+            Application.Current.Resources["Theme1Button"] = Application.Current.Resources["Theme2Button"];
+            Application.Current.Resources["Theme1Background"] = Application.Current.Resources["Theme2Background"];
+        }
+        
+    }
+}
